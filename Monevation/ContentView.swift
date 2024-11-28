49d93332e7615e5802d45ec1e89cc51673ace1f2
khyzhun <sasha.khyzhun@gmail.com>
2024-11-29 01:13:08 +0200

    Initial Commit

---
+++ Monevation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monevation
+//
+//  Created by Alexander Khyzhun on 29.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monevation/MonevationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonevationApp.swift
+//  Monevation
+//
+//  Created by Alexander Khyzhun on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MonevationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
